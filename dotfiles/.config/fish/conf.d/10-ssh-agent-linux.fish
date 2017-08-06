f13dfe9057c887e7786b6c107e5898f121e7bd00
Barret Rennie <barret@brennie.ca>
2017-08-06 10:38:39 -0600

    fish: Fix ssh agent logic on Linux

--- dotfiles/.config/fish/conf.d/10-ssh-agent-linux.fish
+++ dotfiles/.config/fish/conf.d/10-ssh-agent-linux.fish
@@ -1,4 +1,4 @@
-if begin; test (uname) = 'Linux'; and begin; test -z "$SSH_AUTH_SOCK"; or test -S "$SSH_AUTH_SOCK"; end; end
+if begin; test (uname) = 'Linux'; and begin; test -z "$SSH_AUTH_SOCK"; or not test -S "$SSH_AUTH_SOCK"; end; end
     set -Ux SSH_AUTH_SOCK (systemctl --user show-environment | \
                            grep SSH_AUTH_SOCK | \
                            sed -E 's/^.+=//')
