3c5719e261f1f01d5095e8e347d7d1b0aff5caba
Barret Rennie <barret@brennie.ca>
2017-08-29 15:04:24 -0600

    fish: Add rbt post -r and rbt land -r completions

--- dotfiles/.config/fish/completions/rbt.fish
+++ dotfiles/.config/fish/completions/rbt.fish
@@ -15,3 +15,5 @@ function __is_git
 end
 
 complete -c rbt -n '__is_rbt_command land; and __is_git' -f -a '(git branch | cut -c 3-)' -d 'git branch'
+complete -c rbt -n '__is_rbt_command land; and __is_git' -f -s 'r' -a '(rbt status --format="%(id)s")' -d 'review request'
+complete -c rbt -n '__is_rbt_command post' -f -s 'r' -a '(rbt status --format="%(id)s\\x09%(summary)s")'
