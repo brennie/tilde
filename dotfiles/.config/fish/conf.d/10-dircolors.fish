54786e7e71b0629cb257236a39e6dd601bb44dde
Barret Rennie <barret@brennie.ca>
2017-09-30 22:29:01 -0600

    fish: Unconditionally run dircolors in every shell

--- dotfiles/.config/fish/conf.d/10-dircolors.fish
+++ dotfiles/.config/fish/conf.d/10-dircolors.fish
@@ -1,13 +1,1 @@
-if test -z "$_FISH_SHELL_ONCE"
-    if test -z "$TERM"
-        set -x TERM xterm-256color
-        set -x UNSET_TERM 1
-    end
-
-    eval (dircolors -c ~/.dircolors | sed -E 's/^setenv/set -gx/')
-
-    if test $UNSET_TERM -eq 1
-        set -e UNSET_TERM
-        set -e TERM
-    end
-end
+eval (dircolors -c ~/.dircolors | sed -E 's/^setenv/set -gx/')
