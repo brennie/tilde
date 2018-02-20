f3f6048f91b7d60966a9d006329142d867965206
Barret Rennie <barret@brennie.ca>
2018-02-20 16:31:14 -0600

    fish: Add pipenv and lastpass-cli settings to config.fish

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -2,3 +2,5 @@ set -gx EDITOR nvim
 set -gx LESS FRX
 set -gx PAGER less
 set -gx FZF_DEFAULT_COMMAND "rg --files --glob ''"
+set -x PIPENV_SHELL_FANCY 0
+set -x LPASS_PINENTRY /usr/local/bin/pinentry-mac
