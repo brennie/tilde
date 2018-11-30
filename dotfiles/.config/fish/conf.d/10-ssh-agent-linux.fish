b7429bb92603c38ba270a5a92b916cfc6838760a
Barret Rennie <barret@brennie.ca>
2018-11-30 11:51:20 -0500

    fish: Use ksshaskpass when available; add keys to ssh-agent

--- dotfiles/.config/fish/conf.d/10-ssh-agent-linux.fish
+++ dotfiles/.config/fish/conf.d/10-ssh-agent-linux.fish
@@ -3,3 +3,11 @@ if begin; test (uname) = 'Linux'; and begin; test -z "$SSH_AUTH_SOCK"; or not te
                            grep SSH_AUTH_SOCK | \
                            sed -E 's/^.+=//')
 end
+
+if test -z "$_FISH_SHELL_ONCE"
+    ssh-add </dev/null
+
+    if test -e /usr/bin/ksshaskpass
+        set -x SSH_ASKPASS /usr/bin/ksshaskpass
+    end
+end
